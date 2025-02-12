e2678e81b78cdc5e347b3502554e37186213ea7d
BismuthLm <liamislandbay@gmail.com>
2025-02-13 11:55:45 +1300

    up to 1:11 hours

--- scenes/levels/level1.gd
+++ scenes/levels/level1.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+var test_array: Array[String] = ["Test", "Hello", "stuff"]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Icon.rotation_degrees = 90
+	
+	print(test_array[0])
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$Icon.rotation_degrees += 10
+	
+	if $Icon.position.x > 1000:
+		$Icon.pos.x = 0
