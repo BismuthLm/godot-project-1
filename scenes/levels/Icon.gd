c263f6590d9fa2b56789070f424cc07f31471498
BismuthLm <liamislandbay@gmail.com>
2025-02-18 15:17:03 +1300

    About 2:03 into video

--- scenes/levels/Icon.gd
+++ scenes/levels/Icon.gd
@@ -7,17 +7,8 @@ var test_scale: int = 1
 func _ready():
 	pos = Vector2(300, 200)
 	position = pos
-	var test_rotation =45
-	rotation_degrees = test_rotation
-	
-	test_scale =2
-	scale = Vector2(test_scale, test_scale)
-	print($"..".test_array)
-	$"..".test_function
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pos.x += speed * delta
 	position = pos
-	#test_scale += 1
-	scale = Vector2(test_scale, test_scale)
--- scenes/levels/player.gd
+++ scenes/levels/player.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500
+	move_and_slide()
 	
 	if Input.is_action_pressed("primary action"):
 		print("shoot laser")
