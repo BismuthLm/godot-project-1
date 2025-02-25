09b24596cfdb736f0ef44045a2a01619aa45bbe8
BismuthLm <liamislandbay@gmail.com>
2025-02-25 15:16:44 +1300

    alomst 3 hours

--- scenes/levels/level1.gd
+++ scenes/levels/level1.gd
@@ -1,2 +1,13 @@
 extends Node2D
 
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+func _on_gate_player_entered_gate(body):
+	print("player has entered gate")
+	print(body)
+
+func _on_player_laser():
+	print("laser from level")
+
+
+func _on_player_grenade():
+	print("grenade from level")
