e594da4220a5a55208c8e069bad8d83eed96c4cd
BismuthLm <liamislandbay@gmail.com>
2025-03-10 10:38:04 +1300

    FIX THE CAMERA 3:47

--- scenes/levels/level1.gd
+++ scenes/levels/level1.gd
@@ -7,14 +7,16 @@ func _on_gate_player_entered_gate(body):
 	print("player has entered gate")
 	print(body)
 
-func _on_player_laser(pos):
-	var laser = laser_scene.instantiate()
+func _on_player_laser(pos, direction):
+	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
+	laser.direction = direction
 	$projectiles.add_child(laser)
 	
-func _on_player_grenade(pos):
+func _on_player_grenade(pos, direction):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
-	grenade.linear_velocity = Vector2.UP * 100
+	grenade.linear_velocity = direction * grenade.speed
 	$projectiles.add_child(grenade)
 	
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal laser(pos)
-signal grenade(pos)
+signal laser(pos, direction)
+signal grenade(pos, direction)
 
 var can_laser: bool = true
 var can_grenade: bool = true
@@ -13,19 +13,18 @@ func _process(_delta):
 	move_and_slide()
 	
 	look_at(get_global_mouse_position())
-	
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	if Input.is_action_pressed("primary action") and can_laser:
 		var laser_markers = $LaserStartPositions.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
 		$Timer.start()
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		can_grenade = false
 		$GrenadeReloadTimer.start()
 		var pos = $LaserStartPositions.get_children()[0].global_position
-		var player_direction = (get_global_mouse_position() - position).normalized()
 		grenade.emit(pos, player_direction)
 
 func _on_timer_timeout():
